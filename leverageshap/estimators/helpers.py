ae7168acb7d60396b01771dd26db2f79024c3929
R. Teal Witter <rtealwitter@gmail.com>
2026-02-16 08:44:57 -0800

    messing around

--- leverageshap/estimators/helpers.py
+++ leverageshap/estimators/helpers.py
@@ -1,13 +1,38 @@
+import numpy as np
+
 class Game:
-    def __init__(self, model, baseline, explicand):
+    def __init__(self, model, baseline, explicand, subtract_mobius1=False, estimated_phi=None):
         self.model = model
         self.baseline = baseline
         self.explicand = explicand
+        self.subtract_mobius1 = subtract_mobius1            
+        self.estimated_phi = estimated_phi
+        self.zero, self.singletons = self.compute_mobius1()
     
-    def __call__(self, S):
+    def compute_mobius1(self):
+        # Compute the Mobius transform of order 1
+        zero_val = self.model.predict(self.baseline)
+        singletons = np.zeros(self.baseline.shape[1])
+        for j in range(self.baseline.shape[1]):
+            subset_binary = np.zeros(self.baseline.shape[1])
+            subset_binary[j] = 1
+            one_val = self.__call__(subset_binary.reshape(1, -1), insetup=True) - zero_val
+            singletons[j] = one_val
+        return zero_val, singletons
+    
+    def __call__(self, S, insetup=False):
         # S is a m by n binary matrix
         inputs = self.baseline * (1 - S) + self.explicand * S
-        return self.model.predict(inputs)
+        raw_val = self.model.predict(inputs)
+        if self.estimated_phi is not None and not insetup:
+            return raw_val - np.dot(S, self.estimated_phi) - self.zero
+        if self.subtract_mobius1 and not insetup:
+            # Subtract the Mobius transform of order 1
+            singleton_contributions = np.dot(S, self.singletons)
+            return raw_val - singleton_contributions - self.zero
+        if not insetup:
+            return raw_val - self.zero
+        return raw_val
     
     def edge_cases(self):
         v0 = self.model.predict(self.baseline)
--- run_small.py
+++ run_small.py
@@ -2,11 +2,11 @@ import xgboost as xgb
 import leverageshap as ls
 import numpy as np
 
+subtract_mobius1 = False
 
-
-dataset = 'NHANES'
+dataset = 'Communities'
 reps = 3
-size_mults = [.25, .5, 1, 2, 4, 8, 16, 32, 64]
+size_mults = [2, 8, 64, 256]
 
 X, y = ls.load_dataset(dataset)
 n = X.shape[1]
@@ -18,12 +18,14 @@ sample_sizes = [int(n * mult) for mult in size_mults]
 estimator_names = [
     'Regression MSR',
     'Leverage SHAP',
-    'Fourier SHAP'
+#    'Fourier SHAP'
 ]
 
 mse_by_estimator_and_sample_size = {
     name: {sample_size : [] for sample_size in sample_sizes} for name in estimator_names
 }
+
+mse_by_estimator_and_sample_size['Lev->Reg MSR'] = {sample_size : [] for sample_size in sample_sizes}
 
 for i in range(reps):
     model = xgb.XGBRegressor(n_estimators=100, max_depth=4)
@@ -36,12 +38,20 @@ for i in range(reps):
     for estimator_name in estimator_names:
         estimator = ls.estimators[estimator_name]
         for sample_size in sample_sizes:            
-            estimated_shap = estimator(baseline, explicand, model, sample_size).flatten()
-            mse = np.mean((true_shap - estimated_shap)**2)
+            estimated_shap = estimator(baseline, explicand, model, sample_size, subtract_mobius1=subtract_mobius1).flatten()
+            mse = np.mean((true_shap - estimated_shap)**2 / np.mean(true_shap**2))
 
             mse_by_estimator_and_sample_size[estimator_name][sample_size].append(mse)
 
-for estimator_name in estimator_names:
+    lev_estimator = ls.estimators['Leverage SHAP']
+    regmsr_estimator = ls.estimators['Regression MSR']
+    for sample_size in sample_sizes:
+        lev_estimated_shap = lev_estimator(baseline, explicand, model, sample_size, subtract_mobius1=subtract_mobius1).flatten()
+        regmsr_estimated_shap = regmsr_estimator(baseline, explicand, model, sample_size, subtract_mobius1=subtract_mobius1, estimated_phi=lev_estimated_shap).flatten()
+        mse_lev_regmsr = np.mean((true_shap - regmsr_estimated_shap)**2 / np.mean(true_shap**2))
+        mse_by_estimator_and_sample_size['Lev->Reg MSR'][sample_size].append(mse_lev_regmsr)
+
+for estimator_name in mse_by_estimator_and_sample_size.keys():
     for sample_size in sample_sizes:
         mses = mse_by_estimator_and_sample_size[estimator_name][sample_size]
         mean_mse = np.mean(mses)
